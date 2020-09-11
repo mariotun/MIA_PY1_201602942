7e914ff41d60014ef397b6d830b5e16cb2c7aa2e
Mario <oriamnut09@gmail.com>
2020-09-11 13:47:09 -0600

    dandole

--- comandos/com_rep.go
+++ comandos/com_rep.go
@@ -7,6 +7,8 @@ import(
 	//"log"
 	"os"
 	"os/exec"
+	"encoding/binary"
+	"../estructuras"
 )
 
 func REP(path string,ngrafica string,nidgrap string)  {
@@ -132,25 +134,160 @@ func grafica_disco(ubicacion string,path string,extencion string){
 				if (masterb.Mbr_partition[i].Part_status != '1') {
 					if (masterb.Mbr_partition[i].Part_type == 'P'){
 
-						cadena+="     <td height=\"200\" width=\"%.1f\">PRIMARIA <br/>%.1f%c</td>\n",porcentaje_aux,porcentaje_real,"%"
+						cadena+="  <td height=\"200\" width="+"\""+string(porcentaje_aux)+"\""+">PRIMARIA <br/>"+string(porcentaje_real)+"%"+"</td>\n"
 
+						if (i != 3 ) {
+							part1:=masterb.Mbr_partition[i].Part_start + masterb.Mbr_partition[i].Part_size
+							part2:=masterb.Mbr_partition[i+1].Part_start
+							if (masterb.Mbr_partition[i+1].Part_start != -1){
+								if ( (part2-part1)!=0 ){//existe la fragmentacion
+									fragmentacion2:=part2-part1
+									porcentaje_real2:=(fragmentacion2*100)/total
+									porcentaje_aux2:=(porcentaje_real*500)/100
+
+									cadena+="<td height=\"200\" width="+"\""+string(porcentaje_aux2)+"\""+"> LIBRE <br/>"+string(porcentaje_real2)+"%"+"</td>\n"
+								}
+							}
+						}else{
+							part11:=masterb.Mbr_partition[i].Part_start + masterb.Mbr_partition[i].Part_size
+							tam_mbr:=total+int64(binary.Size(masterb))
+
+							if ( (tam_mbr-part11) != 0){
+								libre:=(tam_mbr-part11)+int64(binary.Size(masterb))
+								porcentaje_reall:=(libre*100)/total
+								porcentaje_auxx:=(porcentaje_reall*500)/100
+
+								cadena+="<td height=\"200\" width="+"\""+string(porcentaje_auxx)+"\""+"> LIBRE <br/>"+string(porcentaje_reall)+"%"+"</td>\n"
+
+							}
+						}
 
 					}else{//es una extendida
+
+						cadena+="  <td  height=\"200\" width=\""+string(porcentaje_real)+"\">\n     <table border=\"0\"  height=\"200\" WIDTH=\""+string(porcentaje_real)+"\" cellborder=\"1\">\n"
+						cadena+="  <tr>  <td height=\"60\" colspan=\"15\"> EXTENDIDA </td>  </tr>\n     <tr>\n"
+
+						err2,ebr:=Leer_EBR(path,masterb.Mbr_partition[i].Part_start)
+
+						if ( err2 != nil ){
+							fmt.Println(" Error: No se pudo leer el EBR al intentar graficar. ")
+						}else{
+
+							if ( ebr.Part_size != 0){//por si hay logicas
+
+								var aux estructuras.EbrStr
+								aux=ebr
+						//---------------------------------------------------------------------	
+								for ( aux.Part_next != -1 ) {
+
+									parcial=aux.Part_size
+									porcentaje_real=(parcial*100)/total
+
+									if (porcentaje_real != 0){
+										if ( aux.Part_status != '1'){
+											cadena+=" <td height=\"140\"> EBR </td>\n"
+											cadena+=" <td height=\"140\">LOGICA<br/>"+string(porcentaje_real)+"%"+"</td>\n"
+										
+										}else{//espacion en disco no asignado
+											cadena+=" <td height=\"150\">LIBRE 1 <br/>"+string(porcentaje_real)+"%"+"</td>\n"
+										}
+
+										if ( aux.Part_next == -1){
+											parcial=(masterb.Mbr_partition[i].Part_start + masterb.Mbr_partition[i].Part_size) - ( aux.Part_start + aux.Part_size)
+											porcentaje_real=(parcial*100)/total
+											
+											if ( porcentaje_real != 0) {
+												cadena+="<td height=\"150\">LIBRE 2<br/>"+string(porcentaje_real)+"%"+"</td>\n"
+											}
+											break
+										}/*else{
+											err2,aux=Leer_EBR(path,aux.Part_next)
+										}*/
+
+									}
+
+									err2,aux=Leer_EBR(path,aux.Part_next)
+
+								}//final del for para recorre las logicas
+
+								parcial=aux.Part_size
+								porcentaje_real=(parcial*100)/total
+
+									if (porcentaje_real != 0){
+										if ( aux.Part_status != '1'){
+											cadena+=" <td height=\"140\"> EBR </td>\n"
+											cadena+=" <td height=\"140\">LOGICA<br/>"+string(porcentaje_real)+"%"+"</td>\n"
+										
+										}else{//espacion en disco no asignado
+											cadena+=" <td height=\"150\">LIBRE 1 <br/>"+string(porcentaje_real)+"%"+"</td>\n"
+										}
+
+										if ( aux.Part_next == -1){
+											parcial=(masterb.Mbr_partition[i].Part_start + masterb.Mbr_partition[i].Part_size) - ( aux.Part_start + aux.Part_size)
+											porcentaje_real=(parcial*100)/total
+											
+											if ( porcentaje_real != 0) {
+												cadena+="<td height=\"150\">LIBRE 2<br/>"+string(porcentaje_real)+"%"+"</td>\n"
+											}
+											break
+										}
+
+									}
+						//---------------------------------------------------------------------			
+
+
+							}else{
+								cadena+=" <td height=\"140\"> Ocupado"+string(porcentaje_real)+"%"+"</td>"
+							}
+
+							cadena+="</tr>\n </table>\n </td>\n"
+
+							//ver que no haya fragmentacion
+							if ( i != 3){
+								p1:=masterb.Mbr_partition[i].Part_start + masterb.Mbr_partition[i].Part_size
+								p2:=masterb.Mbr_partition[i+1].Part_start
+
+								if (masterb.Mbr_partition[i+1].Part_start != -1){
+									if ( (p2-p1)!=0){//existe fragmentacion
+										fragmentacion:=p2-p1
+										porcentaje_real:=(fragmentacion*100)/total
+										porcentaje_aux:=(porcentaje_real*500)/100
+
+										cadena+="   <td height=\"200\" width=\""+string(porcentaje_aux)+"\">LIBRE<br/>"+string(porcentaje_real)+"%"+"</td>\n"
+									}
+								}
+							}else{
+								p1:=masterb.Mbr_partition[i].Part_start + masterb.Mbr_partition[i].Part_size
+								mbr_tam:=total + int64(binary.Size(masterb))
+
+								if ( (mbr_tam - p1) != 0){//lo que esta libre
+									libre:=(mbr_tam - p1) + int64(binary.Size(masterb))
+									porcentaje_real:=(libre*100)/total
+									porcentaje_aux:=(porcentaje_real*500)/100
+
+									cadena+="   <td height=\"200\" width=\""+string(porcentaje_aux)+"\">LIBRE<br/>"+string(porcentaje_real)+"%"+"</td>\n"
+								}
+
+							}
+
+						}
+
+
 
 
 
 					}//final del tercer if 
 
 				}else{//es el espacio que no estea asignado
-					cadena+="     <td height=\"200\" width=\"%.1f\">LIBRE <br/> %.1f%c</td>\n"+","+string(porcentaje_aux)+","+string(porcentaje_real)+"%"
+					cadena+="     <td height=\"200\" width="+"\""+string(porcentaje_aux)+"\""+">LIBRE <br/>"+string(porcentaje_real)+"%"+"</td>\n"
 				
 				}//final del segundo if
 
 			}//final del primer if
 
-		}//final del for
+		}//final del for que recorre las particiones
 
-
+		cadena+="</tr> \n  </table>  \n>];\n\n}"
 
 
 
